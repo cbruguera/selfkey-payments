c20b608193c8fec7ef0963445efc80337c88b01d
Carlos Bruguera <cbruguera@gmail.com>
2019-02-06 16:24:47 +0700

    save state

--- contracts/SplitPayment.sol
+++ contracts/SplitPayment.sol
@@ -1,17 +1,19 @@
 pragma solidity ^0.4.23;
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
-contract SplitPayment {
+contract SplitPayment is Ownable {
     using SafeERC20 for StandardToken;
     using SafeMath for uint256;
 
-    uint256 public AFFILIATE_FEE = 10;
+    uint256 public affiliateFee = 10;   // specified as a percentage
     StandardToken public token;
 
     event PaymentInitiated(address sender, address recipient, uint256 amount, bytes32 serviceID);
+    event SetAffiliateFee(uint256 _fee);
 
     constructor(address _token) public {
         require(_token != address(0), "Invalid token address");
@@ -26,12 +28,21 @@ contract SplitPayment {
         uint256 recipientAmount = amount;   // can amount be reused?
 
         if (affiliate != address(0)) {
-            fee = amount.mul(AFFILIATE_FEE).div(100);
+            fee = amount.mul(affiliateFee).div(100);
             recipientAmount = amount.sub(fee);
             token.safeTransferFrom(msg.sender, affiliate, fee);
         }
 
         token.safeTransferFrom(msg.sender, recipient, recipientAmount);
         emit PaymentInitiated(msg.sender, recipient, recipientAmount, serviceID);
+    }
+
+    function setAffiliateFee(uint256 _fee)
+        public
+        onlyOwner
+    {
+        require(_fee <= 100, "fee must be specified as a percentage: 0 <= 100");
+        affiliateFee = _fee;
+        emit SetAffiliateFee(_fee);
     }
 }
